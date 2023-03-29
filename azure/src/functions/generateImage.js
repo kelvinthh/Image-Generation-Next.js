820b1339f115fc52bd78fa49684b93a943170a2b
Kelvin Tam <3705865+kelvinthh@users.noreply.github.com>
2023-03-29 18:34:19 -0400

    Generate image from Dall-E
    - Add helper for generating SAS token
    - Add API routes for generating SAS Token and making img gen request

---
+++ azure/src/functions/generateImage.js
@@ -0,0 +1,58 @@
+const { app } = require("@azure/functions");
+const openai = require("../../lib/openai");
+const axios = require("axios");
+const generateSASToken = require("../../lib/generateSASToken");
+
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+const accountName = process.env.ACCOUNT_NAME;
+const containerName = "images";
+
+app.http("generateImage", {
+  method: ["POST"],
+  authLevel: "anonymous",
+  handler: async (request) => {
+    const { prompt } = await request.json();
+
+    console.log(`Prompt: ${prompt}`);
+
+    const response = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024",
+    });
+
+    image_url = response.data.data[0].url;
+
+    console.log("Image URL: " + image_url);
+
+    // Download the image as an ArrayBuffer
+    const res = await axios.get(image_url, { responseType: "arraybuffer" });
+
+    const arrayBuffer = res.data;
+
+    sasToken = await generateSASToken();
+
+    const blobServiceClient = new BlobServiceClient(
+      `https://${accountName}.blob.core.windows.net?${sasToken}`
+    );
+
+    console.log("Account Name: " + accountName);
+    console.log("sasToken: " + sasToken);
+
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+    // Generate current timestamp
+    const timestamp = new Date().getTime();
+    const file_name = `${prompt}_${timestamp}.png`;
+
+    const blockBlobClient = containerClient.getBlockBlobClient(file_name);
+
+    try {
+      await blockBlobClient.uploadData(arrayBuffer);
+      console.log("File has been successfully uploaded.");
+    } catch (e) {
+      console.error("Failed to upload:", e.message);
+    }
+  },
+});
