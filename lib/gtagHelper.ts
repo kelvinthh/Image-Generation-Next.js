69b39c12f40655f3e310a418f927cf7378026cf5
Kelvin H H Tam <hoheitam@alumni.vtc.edu.hk>
2023-08-15 13:34:15 -0400

    Update gtagHelper.ts
    - Fix gtag type error

--- lib/gtagHelper.ts
+++ lib/gtagHelper.ts
@@ -1,5 +1,16 @@
-export const pageview = (GA_MEASUREMENT_ID : string | undefined, url : string) => {
-    window.gtag("config", GA_MEASUREMENT_ID, {
-        page_path: url,
-    });
+export const pageview = (
+  GA_MEASUREMENT_ID: string | undefined,
+  url: string
+) => {
+  window.gtag("config", GA_MEASUREMENT_ID, {
+    page_path: url,
+  });
 };
+
+// For error Type error: Property 'gtag' does not exist on type 'Window & typeof globalThis'.
+// https://stackoverflow.com/questions/56457935/typescript-error-property-x-does-not-exist-on-type-window
+declare global {
+  interface Window {
+    gtag: any;
+  }
+}
